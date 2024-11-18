f77314e8c1a92ad94a180d8fe3e733d86b4d645b
jollygrin <JollyGrin@protonmail.com>
2024-11-19 00:32:19 +0100

    fun

--- internal/api/stuff_test.go
+++ internal/api/stuff_test.go
@@ -1,1 +1,38 @@
 package api_test
+
+import (
+	"testing"
+	"time"
+)
+
+type Foo struct {
+	LastTouched time.Time
+}
+
+func (f *Foo) UpdateTouched() { // Methods
+	f.LastTouched = time.Now()
+}
+
+func (f Foo) Error() string {
+	return "no!"
+}
+
+func TestMutate(t *testing.T) {
+
+	// In typescript
+	// type Reader interface {
+	// 	Size: number
+	// 	Read(i: string ): string
+	// }
+
+	// slices, maps
+
+	// io.Reader
+	var f error = Foo{}
+
+	var _ = f
+}
+
+func Mutate(v []int) { // Functions
+	v[0] = 2
+}
