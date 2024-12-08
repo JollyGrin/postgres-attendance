d844201e8374fc7d683ebfd435d55059556044cc
jollygrin <JollyGrin@protonmail.com>
2024-12-08 04:46:28 +0100

    working gant bars

--- dcl-attend-frontend/src/app/page.tsx
+++ dcl-attend-frontend/src/app/page.tsx
@@ -33,11 +33,23 @@ function getDuration(start: string, end: string) {
 }
 
 const RecordList = () => {
-  const searchDate = "2024-12-03";
+  const searchDate = "2024-12-08";
   const { data } = useQuery({
     queryKey: ["duration", searchDate],
     queryFn: () => getDayDuration(searchDate),
   });
+
+  const attendees = data
+    ?.filter(
+      (record, index, self) =>
+        self.findIndex((rec) => rec.address === record.address) === index,
+    )
+    ?.map((record) => ({
+      address: record.address,
+      durations: data.filter((rec) => rec.address === record.address),
+    }));
+
+  console.log({ attendees });
 
   const [firstStart] =
     data?.sort((a, b) => {
@@ -60,12 +72,13 @@ const RecordList = () => {
 
   return (
     <>
-      {data
+      {attendees
         // ?.splice(4, 1)
         ?.map((record) => (
           <Record
-            key={record.address + record.enter_time}
-            {...record}
+            key={record.address}
+            address={record.address}
+            durations={record.durations}
             first={first}
             last={last}
             totalDuration={durationMs}
@@ -75,30 +88,40 @@ const RecordList = () => {
   );
 };
 
-const Record = (
-  record: AttendRecord & {
-    first?: number;
-    last?: number;
-    totalDuration?: number;
-  },
-) => {
+const Record = (record: {
+  address: string;
+  durations: AttendRecord[];
+  first?: number;
+  last?: number;
+  totalDuration?: number;
+}) => {
   if (!record.first || !record.last || !record?.totalDuration) return null;
   const { data } = useDclPlayer(record.address);
   const [avatar] = data?.avatars ?? [];
   const pfp = avatar?.avatar.snapshots.face256;
 
-  const percentageWidth = record.duration / record.totalDuration;
-  const startDifference = new Date(record.enter_time).getTime() - record.first;
+  const duration = record.durations[0];
+
+  const percentageWidth = duration.duration / record.totalDuration;
+  const startDifference =
+    new Date(duration.enter_time).getTime() - record.first;
   const percentageLeft = startDifference / 1000 / record.totalDuration;
-  console.table({
-    total: record.totalDuration,
-    duration: record.duration,
-    percentageWidth,
-    startTime: new Date(record.enter_time).getTime(),
-    firstTime: record.first,
-    startDifference: startDifference / 1000,
-    percentageLeft,
-  });
+
+  function findLeft(enterTime: string) {
+    if (!enterTime || !record.first || !record.totalDuration) return 0;
+    const startDifference = new Date(enterTime).getTime() - record.first;
+    const percentageLeft = startDifference / 1000 / record.totalDuration;
+    return percentageLeft;
+  }
+  // console.table({
+  //   total: record.totalDuration,
+  //   duration: record.duration,
+  //   percentageWidth,
+  //   startTime: new Date(record.enter_time).getTime(),
+  //   firstTime: record.first,
+  //   startDifference: startDifference / 1000,
+  //   percentageLeft,
+  // });
 
   return (
     <div className="grid grid-cols-[3rem_5rem_1fr] items-center">
@@ -116,15 +139,25 @@ const Record = (
 
       <p>{record.address.substring(0, 6)}...</p>
       <div className="bg-green-50 min-h-5 w-full relative">
-        <div
-          className="absolute bg-green-400 h-full"
-          style={{
-            width: `${percentageWidth * 100}%`,
-            left: `${percentageLeft * 100}%`,
-          }}
-        ></div>
-        {/* add bar that starts and ends at time */}
+        {record?.durations?.map((dur) => (
+          <ChartLine
+            width={dur.duration / (record?.totalDuration ?? 1)}
+            left={findLeft(dur?.enter_time)}
+          />
+        ))}
       </div>
     </div>
+  );
+};
+
+const ChartLine = (props: { width: number; left: number }) => {
+  return (
+    <div
+      className="absolute bg-green-400 h-full rounded-xl"
+      style={{
+        width: `${props.width * 100}%`,
+        left: `${props.left * 100}%`,
+      }}
+    ></div>
   );
 };
