3657d73da1117a741cc806854e593e64899520c6
jollygrin <JollyGrin@protonmail.com>
2024-12-10 03:01:50 +0100

    update nav

--- dcl-attend-frontend/src/app/page.tsx
+++ dcl-attend-frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { IconChevDown } from "@/components/atoms/icons";
 import { useState } from "react";
 
 export default function Home() {
@@ -20,10 +21,11 @@ export default function Home() {
         className="border-border border-r-2 group-hover:[grid-template-columns:8rem_1fr]"
       ></div>
       <div className="flex flex-col">
-        <div className="border-border border-b-2 min-h-16 px-4 flex items-center gap-3">
+        <div className="border-border border-b-2 min-h-16 px-4 flex items-center gap-3 font-mono">
           <span>org</span>
           <span className="text-border font-bold">/</span>
           <span>innkeeper</span>
+          <IconChevDown color="var(--color-border)" />
         </div>
       </div>
     </div>
---
+++ dcl-attend-frontend/src/components/atoms/icons/index.ts
@@ -0,0 +1,1 @@
+export { IconChevDown } from "./IconChevDown";
