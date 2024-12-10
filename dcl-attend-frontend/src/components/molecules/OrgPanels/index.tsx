e4c038b7b2bca7bcdaad10fbdeb3ec29df054b9b
jollygrin <JollyGrin@protonmail.com>
2024-12-10 03:54:55 +0100

    responnsive

--- dcl-attend-frontend/src/components/molecules/OrgPanels/index.tsx
+++ dcl-attend-frontend/src/components/molecules/OrgPanels/index.tsx
@@ -9,7 +9,7 @@ export const OrgPanels = (props: {
   }[];
 }) => {
   return (
-    <div className="flex justify-between">
+    <div className="flex flex-col justify-between md:flex-row gap-4 md:gap-0">
       <div className="bg-panel p-4 border-border border-2 rounded-lg flex items-center gap-4 text-xl">
         <img src={props.org.img} height="40px" width="40px" />
         <p>{props.org.name}</p>
@@ -17,7 +17,7 @@ export const OrgPanels = (props: {
 
       <div className="flex gap-4 font-mono">
         {props.locations?.map((loc) => (
-          <div className="bg-panel p-4 border-border border-2 rounded-lg flex items-center gap-4 text-xl">
+          <div className="bg-panel p-4 border-border border-2 rounded-lg items-center gap-4 text-xl flex flex-col md:flex-row w-full md:w-fit">
             <img src={`${VERSE[loc.name]?.img}`} height="40px" width="40px" />
             <div>
               <p className="text-xs">{VERSE[loc.name]?.name}</p>
