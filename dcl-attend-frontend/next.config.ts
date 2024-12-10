59e062ad03ca168a94ab67a8f747841a244d26d4
jollygrin <JollyGrin@protonmail.com>
2024-12-10 04:13:53 +0100

    update config

--- dcl-attend-frontend/next.config.ts
+++ dcl-attend-frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: "out",
+  // Required for GitHub Pages deployment - turned off when deploying to cv.dean.lol
+  // basePath: process.env.NODE_ENV === "production" ? "/deanlol-cv" : "/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
